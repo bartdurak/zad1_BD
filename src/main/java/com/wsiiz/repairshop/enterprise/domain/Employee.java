b1e92257a62750e6da10764ef2efc4fc855c1abc
Bart D <budownictwa@zoho.com>
2022-06-11 22:33:19 +0200

    startA zad1

--- src/main/java/com/wsiiz/repairshop/enterprise/domain/Employee.java
+++ src/main/java/com/wsiiz/repairshop/enterprise/domain/Employee.java
@@ -1,4 +1,8 @@
 package com.wsiiz.repairshop.enterprise.domain;
 
+import com.wsiiz.repairshop.foundation.domain.AbstractFactory;
+import org.springframework.stereotype.Component;
 public class Employee {
+
+
 }
