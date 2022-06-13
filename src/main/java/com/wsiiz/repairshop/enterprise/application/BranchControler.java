c84514051d568ca08c13ab7c7f447dfd67894d87
Bart D <batq.ar@zoho.com>
2022-06-13 06:40:24 +0200

    Address_a zad1

--- src/main/java/com/wsiiz/repairshop/enterprise/application/BranchControler.java
+++ src/main/java/com/wsiiz/repairshop/enterprise/application/BranchControler.java
@@ -1,55 +1,54 @@
 package com.wsiiz.repairshop.enterprise.application;
 
-import com.wsiiz.repairshop.enterprise.domain.branch.Branch;
-import com.wsiiz.repairshop.enterprise.domain.branch.BranchAddress;
-import com.wsiiz.repairshop.enterprise.domain.branch.BranchRepository;
-import com.wsiiz.repairshop.enterprise.domain.branch.ParentBranch;
-import com.wsiiz.repairshop.enterprise.domain.employee.Employee;
-import com.wsiiz.repairshop.enterprise.domain.employee.EmployeeRepository;
-import com.wsiiz.repairshop.servicing.domain.service.Service;
-import com.wsiiz.repairshop.servicing.domain.servicerequest.RequestType;
+import com.wsiiz.repairshop.enterprise.domain.Branch;
+import com.wsiiz.repairshop.enterprise.domain.BranchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+
+@RestController
 
 public class BranchControler {
     @Autowired
     BranchRepository branchRepository;
 
-    @GetMapping("/branches")
-    public ResponseEntity<List<Branch>> getMany(
-            @RequestParam(value = "city", required = false) BranchAddress city,
-            @RequestParam(value = "parent_branch_name", required = false) ParentBranch parentBranchName){
-        return ResponseEntity.ok(branchRepository.findBySelectionCriteria(city, parentBranchName));
+    @GetMapping ("/branches")
+    public ResponseEntity<List<Branch>> getMany (
+            @RequestParam (value = "locality", required = false) String locality,
+            @RequestParam (value = "idDep", required = false) Long idDep)
+    {
+        return ResponseEntity.ok(branchRepository.findByAddressLocalityOrIdDep(locality, idDep));
     }
 
     @GetMapping("/branches/{id}")
-    public ResponseEntity<Branch> getOne(@PathVariable("id") String id) {
-
+    public ResponseEntity<Branch> getOne (
+            @PathVariable ("id") Long id)
+    {
         Optional<Branch> branch = branchRepository.findById(id);
+        return branch.map(ResponseEntity::ok).orElseGet(()->ResponseEntity.notFound().build());
 
-        return branch.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping("/branches")
-    public ResponseEntity<Branch> addNew(@RequestBody Branch branch) {
-        branch.getEmployees().forEach(r->r.setBranch(branch));
+    public ResponseEntity<Branch> addNew (@RequestBody Branch branch)
+    {
         return ResponseEntity.created(null).body(branchRepository.save(branch));
     }
 
     @DeleteMapping("/branches/{id}")
-    public ResponseEntity<Branch> remove(@PathVariable("id") String id) {
-
-        Optional<Branch> branch = branchRepository.findById(id);
-
-        if (branch.isPresent()) {
+    public ResponseEntity<Branch> remove (@PathVariable("id") Long id ){
+        Optional <Branch> branch = branchRepository.findById(id);
+        if (branch.isPresent())
+        {
             branchRepository.deleteById(id);
             return ResponseEntity.ok(branch.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
 
+        }
+        else
+            return ResponseEntity.notFound().build();
+
+    }
 }
--- src/main/java/com/wsiiz/repairshop/enterprise/application/EmployeeControler.java
+++ src/main/java/com/wsiiz/repairshop/enterprise/application/EmployeeControler.java
@@ -1,18 +1,60 @@
 package com.wsiiz.repairshop.enterprise.application;
-import com.wsiiz.repairshop.enterprise.domain.branch.Branch;
-import com.wsiiz.repairshop.enterprise.domain.branch.BranchAddress;
-import com.wsiiz.repairshop.enterprise.domain.branch.BranchRepository;
-import com.wsiiz.repairshop.enterprise.domain.branch.ParentBranch;
-import com.wsiiz.repairshop.enterprise.domain.employee.Employee;
-import com.wsiiz.repairshop.enterprise.domain.employee.EmployeeRepository;
-import com.wsiiz.repairshop.servicing.domain.service.Service;
-import com.wsiiz.repairshop.servicing.domain.servicerequest.RequestType;
-import org.springframework.beans.factory.annotation.Autowired;
 
+import com.wsiiz.repairshop.enterprise.domain.Employee;
+import com.wsiiz.repairshop.enterprise.domain.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.Optional;
-
+@RestController
 
 public class EmployeeControler {
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public ResponseEntity<List<Employee>> getMany (
+            @RequestParam(value = "idDep", required = false) Long idDepartment)
+    {
+        if (idDepartment == null) {
+            return ResponseEntity.ok(employeeRepository.findAll());
+
+        }
+        else {
+            return ResponseEntity.ok(employeeRepository.findByidDepartment(idDepartment));
+
+        }
+
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getOne (
+            @PathVariable("id") Long id)
+    {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        return employee.map(ResponseEntity::ok).orElseGet(()->ResponseEntity.notFound().build());
+
+    }
+
+    @PostMapping("/employees")
+    public ResponseEntity<Employee> addNew (@RequestBody Employee employee)
+    {
+        return ResponseEntity.created(null).body(employeeRepository.save(employee));
+    }
+
+    @DeleteMapping("/employees{id}")
+    public ResponseEntity<Employee> remove (@PathVariable("id") Long id ){
+        Optional <Employee> employee = employeeRepository.findById(id);
+        if (employee.isPresent())
+        {
+            employeeRepository.deleteById(id);
+            return ResponseEntity.ok(employee.get());
+
+        }
+        else
+            return ResponseEntity.notFound().build();
+
+    }
 }
